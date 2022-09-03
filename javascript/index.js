65e1b1fe8a6ed5955fcbb8f0682fa91a7b7a9540
Christopher Guevarra <cjg73902@gmail.com>
2022-09-03 14:28:11 -0400

    Refactor code to allow user to input desired grid size, and ability to clear grid

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,57 @@
-const CONTAINER = document.querySelector(`#container`);
+function cellHoverHandler(e) {
+  e.target.classList.add(`cell-hovered`);
+}
 
-function createCell(parentElement,denominator){
-    const newCell = document.createElement(`div`);
-    const parentHeight = parentElement.offsetHeight;
-    const newCellHeight = `${parentHeight/denominator}px`;
-    newCell.style.width = newCellHeight;
-    newCell.style.height = newCellHeight;
-    newCell.style.backgroundColor="red";
-    newCell.classList.add('cell');
-    parentElement.appendChild(newCell);
+function createCell(parentElement, denominator) {
+  const newCell = document.createElement(`div`);
+  const parentHeight = parentElement.offsetWidth;
+  const newCellHeight = `${Math.floor(parentHeight / denominator)}px`;
+  newCell.style.width = newCellHeight;
+  newCell.style.height = newCellHeight;
+  newCell.classList.add("cell");
+  newCell.addEventListener("mouseover", (e) => {
+    cellHoverHandler(e);
+  });
+  parentElement.appendChild(newCell);
 }
-let gridSize = 4
-for(let i=0;i<(gridSize*gridSize);i++){
-    createCell(CONTAINER,gridSize);
+
+function createGrid(gridSize) {
+  if(gridSize>=100){return true};
+  const NEW_CONTAINER = document.createElement("div");
+  const BODY = document.querySelector("body");
+  NEW_CONTAINER.classList.add(`container`);
+  BODY.appendChild(NEW_CONTAINER);
+  const CONTAINER = document.querySelector(".container");
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    createCell(CONTAINER, gridSize);
+  }
 }
+
+function removeGrid() {
+  const BODY = document.querySelector("body");
+  const CONTAINER = document.querySelector(".container");
+  if (CONTAINER !== null) {
+    BODY.removeChild(CONTAINER);
+  }
+}
+
+function createButtonOnClickHandler() {
+  removeGrid();
+  const gridSize = prompt("Please enter valid grid size below 100");
+  createGrid(gridSize);
+}
+
+function clearButtonOnClickHandler() {
+  const cells = document.querySelectorAll(".cell");
+  if (cells.length == 0) {
+    return;
+  }
+  cells.forEach((cell)=>{
+    cell.classList.remove('cell-hovered');
+  })
+}
+const CREATE_BUTTON = document.querySelector("#createGridButton");
+CREATE_BUTTON.addEventListener("click", createButtonOnClickHandler);
+
+const CLEAR_BUTTON = document.querySelector("#clearGridButton");
+CLEAR_BUTTON.addEventListener("click", clearButtonOnClickHandler);
